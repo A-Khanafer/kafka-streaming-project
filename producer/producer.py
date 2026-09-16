413f8ead0d5b9394f91dfb61e72f1c714cda0116
A-Khanafer <ahmad.khanafer.191@gmail.com>
2026-09-16 15:27:20 -0400

    feat: add Kafka producer

--- producer/producer.py
+++ producer/producer.py
@@ -1,1 +1,21 @@
-#Producer is going to send the events into Kafka.
+from confluent_kafka import Producer
+
+# This creates the Kafka producer and tells it where Kafka is.
+# bootstrap.servers is basically asking "Where can I find at least one Kafka broker so I can connect to the Kafka cluster?"
+# In our case, I exposed Kafka port 9092. So we use localhost:9092.
+producer = Producer({
+    "bootstrap.servers" : "localhost:9092"
+})
+
+# Sends "hellows kafka" to the Kafka topic we created earlier "network-events".
+producer.produce(
+    topic="network-events",
+    value="hellows kafka"   
+)
+
+# Now this is important because Kafka producers works ASYNCHRONOUSLY. 
+# produce() doesn't necessarily mean that the message has reached Kafka. It's more like it queued the message to be sent.
+# Flush() waits until pending messages have actually been delivered before python exits.
+producer.flush()
+
+print("Message sent")
